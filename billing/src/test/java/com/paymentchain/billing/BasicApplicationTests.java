b4dd0b0f42df2204d64b6a93f1a413c4e24aba3f
Ignacio-CI <ignacio.cerda.i@gmail.com>
2024-07-16 18:07:35 +0200

    slack and automation

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
  
  @Test
  public void contextLoads() {
- 	    String meessage= "default message cambio test devops: slack";
+ 	    String meessage= "default message cambio test devops: slack y automation";
  	    Assert.assertNotNull(meessage);
  }
  
